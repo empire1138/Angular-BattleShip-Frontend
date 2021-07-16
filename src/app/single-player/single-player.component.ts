00a8c03abb092df7dbd2064b39194454564f5265
Gabe juarez <47907878+empire1138@users.noreply.github.com>
2021-07-16 09:17:16 -0700

    learning Angular View Manipulation

--- src/app/single-player/single-player.component.ts
+++ src/app/single-player/single-player.component.ts
@@ -1,40 +1,49 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Directive, ElementRef, OnInit, Query, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-single-player',
   templateUrl: './single-player.component.html',
   styleUrls: ['./single-player.component.css']
 })
-export class SinglePlayerComponent implements OnInit {
+export class SinglePlayerComponent implements OnInit, AfterViewInit{
 
-  // @ViewChild('userGrid') userGrid!: ElementRef;
-  // @ViewChild('computerGrid') computerGrid!: ElementRef; 
+  @ViewChild("userGrid") userGrid!: ElementRef;
+  @ViewChild("computerGrid") computerGrid!: ElementRef; 
 
   width = 10;
   userSquares:any = []
   computerSquares:any = []
 
 
-  userGrid = document.querySelector('.grid-user')
-  computerGrid = document.querySelector('.grid-computer')
+  // userGrid = document.querySelector('.grid-user')
+  
+  // computerGrid = document.querySelector('.grid-computer')
+  
+  ngAfterViewInit() {
+
+   // console.log(this.userGrid.nativeElement.querySelector('.grid-user'));
+    //console.log(this.renderer.selectRootElement(this.userGrid));
+
+    this.createBoard(this.renderer.selectRootElement(this.userGrid), this.userSquares);
+    this.createBoard(this.renderer.selectRootElement(this.computerGrid), this.computerSquares);
+    
+  }
 
   constructor(private renderer:Renderer2) { }
 
   ngOnInit(): void {
-    this.createBoard(this.userGrid, this.userSquares);
-    this.createBoard(this.computerGrid, this.computerSquares);
+  
   }
 
-  
   //Calling the functions
-  
-
 
   //Making the functions for the game
   createBoard(grid: any, squares: any) {
+    console.log(grid);
+    console.log(squares);
     for (let i = 0; i < (this.width* this.width); i++) {
-      const square = this.renderer.createElement("div")
-      square.dataset.id = i
+      const square = this.renderer.createElement('div');
+      square.dataset.id = i;
       // grid.appendChild(square)
       this.renderer.appendChild(grid, square);
       squares.push(square)
